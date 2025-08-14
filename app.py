412e92f561c37c8dbadee6159aabc5f58da57c29
acaminata <acamino@copec.cl>
2025-08-13 22:20:16 -0400

    Create app.py
    Initial Set-Up.

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Configuración básica de la página
+st.set_page_config(page_title="Hola mundo", page_icon="👋")
+
+# Contenido
+st.title("¡Hola mundo! 👋")
+st.write("Esta es mi primera app de Streamlit publicada en la web.")
